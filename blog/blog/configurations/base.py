65d61ff956fddc168512f2f4fc7b12612cbb0fd7
Juliana98-git <juliananardelli9@gmail.com>
2024-10-20 19:27:14 -0300

    cambios

--- blog/blog/configurations/base.py
+++ blog/blog/configurations/base.py
@@ -15,7 +15,7 @@ import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent.parent
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
@@ -107,7 +107,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # produccion
 # cuando utilizamos collectstatic
-STATIC_ROOT = [os.path.join(BASE_DIR, 'staticfiles')]
+STATIC_ROOT = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
